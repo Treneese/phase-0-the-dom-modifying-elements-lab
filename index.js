f76897782d4ec2b3d8f11d2dc309f72e01336d42
treneese <treneesejohnson9012@gmail.com>
2024-03-24 17:19:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+const main =document.querySelector('main')
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id','victory');
+
+
+console.log (newHeader)
+
+const li = document.createElement("li");
+
+newHeader.textContent = "Treneese is the champion";
+
+
+// main.remove().textContent;
+
+// let yourName = "Treneese";{
+// el.innerHTML = (yourName); 
+
+// newHeader = "<h1><h1>";
+// el.innerHTML = (newHeader);
+// }
+// const element = document.createElement("h1");
+
+//   newHeader.innerHTML = `<h1><a href="#">victory ${newHeader.children.length = 1}</a></h1>`;
+
+
